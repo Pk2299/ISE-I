28638121e55b63131b634474fe360ce0fec03610
Nikhil1227 <nikhildontul143.com>
2023-06-07 10:36:17 +0530

    Create fibo file

---
+++ Fibo.java
@@ -0,0 +1,16 @@
+
+public class Fibo
+{
+    public static void main(String[] args) 
+    {
+        int n, a = 0, b = 0, c = 1;
+        System.out.print("Fibonacci Series:");
+        for(int i = 1; i <= 100; i++)
+        {
+            a = b;
+            b = c;
+            c = a + b;
+            System.out.print(a+" ");
+        }
+    }
+}
